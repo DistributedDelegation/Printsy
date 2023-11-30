14cd386f0f776aed3f1e8bd7f5f8d6e92ea90639
MichaelMoyles <93867352+MichaelMoyles@users.noreply.github.com>
2023-11-30 17:30:40 +0000

    WIP Gallery Service Frontend

--- Frontend/src/components/Gallery.jsx
+++ Frontend/src/components/Gallery.jsx
@@ -1,11 +1,10 @@
-// TO DO: Create frontend and connect to Gallery once api-gateway is set up
 import { useState } from 'react'
 
 const Gallery = () => {
 
     const {setGalleryImages, galleryImages} = useState("")
     const fetchRandomImage = () => {
-        fetch('http://localhost:8085/graphql', {
+        fetch('http://localhost:8086/graphql', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({
@@ -26,7 +25,7 @@ const Gallery = () => {
 
     return (
         <div>
-            <button onClick={fetchRandomImage()}></button>
+            <button onClick={fetchRandomImage()}>Click Me!</button>
             {galleryImages && (
                 <p>Gallery Images Displayed</p>
             )}
