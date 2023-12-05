0a39fbb44f541dc47e3bbcb514c98a925abc8606
MichaelMoyles <93867352+MichaelMoyles@users.noreply.github.com>
2023-12-05 13:46:12 +0000

    WIP trying to connect to mongodb backend

--- Frontend/src/components/Gallery.jsx
+++ Frontend/src/components/Gallery.jsx
@@ -1,31 +1,103 @@
 import { useState } from 'react'
 
 const Gallery = () => {
-
     const {setGalleryImages, galleryImages} = useState("")
-    const fetchRandomImage = () => {
-        fetch('http://localhost:8086/graphql', {
+    
+    // Define GraphQL endpoint
+    let mongoGraphqlEndpoint = "http://localhost:8086/graphql";
+
+    const sanitizeInput = (input) => {
+        // Basic sanitation just to demonstrate. You might need more complex logic.
+        return input.trim().replace(/[^a-zA-Z0-9-_.:/?&=%]/g, '')
+    }
+
+    const generateImage = () => {
+        const imageUrl = document.getElementById('imageUrlInput').value;
+        const sanitizedUrl = sanitizeInput(imageUrl);
+      
+        console.log(sanitizedUrl);
+      
+        const query = JSON.stringify({
+          query: "query($url: String!) { fetchImage(url: $url) }",
+          variables: {
+            url: sanitizedUrl
+          }
+        });
+      
+        console.log(`Sending the following query to both ${mongoGraphqlEndpoint}: ${query}`);
+      
+        // Send the request to the GraphQL endpoint
+        fetch(mongoGraphqlEndpoint, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: query
+        })
+          .then(response => response.json())
+          .then(data => {
+            const imageUrl = data.data.fetchImage;
+            const imageDisplay = `<p>Image URL: ${imageUrl}</p><img id="imageToSave" src=${imageUrl}>`;
+            document.getElementById('mongoImageContainer').innerHTML = imageDisplay
+          })
+          .catch(error => {
+            console.error('Error fetching the image:', error);
+          });
+    }
+
+    // Function to save an image via a GraphQL mutation
+    const saveImage = () => {
+        const imageElement = document.getElementById('imageToSave');
+        if (imageElement) {
+        // Extract the Base64 part of the data URL
+        const imageUrl = imageElement.src;
+    
+        // Define the GraphQL mutation
+        const mutation = JSON.stringify({
+            query: `mutation($imageUrl: String!) {
+            createImage(imageUrl: $imageUrl)
+            }`,
+            variables: {
+            imageUrl: imageUrl
+            }
+        });
+    
+        // Send the request to the GraphQL endpoint
+        fetch(mongoGraphqlEndpoint, {
             method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({
-                query: `{
-                randomImage {
-                    id
-                    link
-                }
-            }`
-            }),
+            headers: {
+            'Content-Type': 'application/json'
+            },
+            body: mutation
         })
             .then(response => response.json())
             .then(data => {
-                setGalleryImages(data)
+              console.log(data);
+            console.log('Image saved, ID:', data.data.createImage);
+            const sucessDisplay = `<p>Image saved in MongoDB, ID: ${data.data.createImage}</p>`;
+            document.getElementById('mongoSuccessContainer').innerHTML = sucessDisplay;
             })
-            .catch(error => console.error('Error fetching data: ', error));
+            .catch(error => {
+            console.error('Error saving the image:', error);
+            });
+        } else {
+        console.error('No image to save.');
+        }
     }
 
     return (
         <div>
-            <button onClick={fetchRandomImage()}>Click Me!</button>
+            <input type="text" id="imageUrlInput" placeholder="Enter Image URL"/>
+            <button id="generateImageBtn" onClick={generateImage}>Get Image</button>
+            <button id="saveImageBtn" onClick={saveImage}>Save Image</button>
+
+            <div id="results" style={{marginTop: "30px", display: "flex", flexDirection: "row"}}>
+                <div id="mongo-results" style={{marginRight: "30px"}}>
+                    <div id="mongoImageContainer"></div>
+                    <div id="mongoSuccessContainer"></div>
+                </div>
+            </div>
+
             {galleryImages && (
                 <p>Gallery Images Displayed</p>
             )}
