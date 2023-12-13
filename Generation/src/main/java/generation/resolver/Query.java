bf56442b192a57c990b9dd9f383d857df3777d70
florian-lechner <38959593+florian-lechner@users.noreply.github.com>
2023-12-13 01:04:07 +0000

    FetchFeatures

--- Generation/src/main/java/generation/resolver/Query.java
+++ Generation/src/main/java/generation/resolver/Query.java
@@ -1,6 +1,7 @@
 package generation.resolver;
 
 import generation.model.Image;
+import generation.model.Feature;
 import generation.repository.ImageMySQLRepository;
 
 import org.springframework.graphql.data.method.annotation.Argument;
@@ -10,6 +11,7 @@ import org.springframework.stereotype.Controller;
 
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
@@ -25,6 +27,22 @@ public class Query {
     @Autowired
     public Query(ImageMySQLRepository sqlRepository) {
         this.sqlRepository = sqlRepository;
+    }
+
+    @QueryMapping
+    public List<Feature> fetchFeatures() {
+        return Arrays.asList(
+            new Feature("Style", Arrays.asList("Abstract", "Realistic", "Cartoon", "Minimalist")),
+            new Feature("Color Palette", Arrays.asList("Vibrant", "Pastel", "Monochrome", "Earth Tones")),
+            new Feature("Theme", Arrays.asList("Nature", "Urban", "Space", "Fantasy")),
+            new Feature("Mood", Arrays.asList("Joyful", "Serene", "Mysterious", "Energetic")),
+            new Feature("Subject Focus", Arrays.asList("Animals", "People", "Landscapes", "Objects")),
+            new Feature("Composition", Arrays.asList("Centralized", "Asymmetrical", "Layered", "Framed")),
+            new Feature("Texture", Arrays.asList("Smooth", "Rough", "Patterned", "Glowing")),
+            new Feature("Era", Arrays.asList("Vintage", "Contemporary", "Futuristic", "Historical")),
+            new Feature("Cultural Influence", Arrays.asList("Asian", "Western", "African", "Indigenous")),
+            new Feature("Graphic Elements", Arrays.asList("Geometric Shapes", "Floral Patterns", "Gradients", "Line Art"))
+        );
     }
 
     /*
