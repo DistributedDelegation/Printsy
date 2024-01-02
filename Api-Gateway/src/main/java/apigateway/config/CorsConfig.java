c1679a74537602ff20e76eabea475f72ea8e388f
tlopesx <tlopes123x@gmail.com>
2024-01-02 11:33:01 -0500

    adding 127.0.0.1 to cors allowed origins

--- Api-Gateway/src/main/java/apigateway/config/CorsConfig.java
+++ Api-Gateway/src/main/java/apigateway/config/CorsConfig.java
@@ -15,7 +15,7 @@ public class CorsConfig {
     public CorsWebFilter corsWebFilter() {
         final CorsConfiguration corsConfig = new CorsConfiguration();
 
-        corsConfig.setAllowedOrigins(List.of("http://localhost:5173")); // specify your frontend application URL
+        corsConfig.setAllowedOrigins(List.of("http://localhost:5173","http://127.0.0.1:5173")); // specify your frontend application URL
         corsConfig.setMaxAge(3600L); // 1 hour
         corsConfig.addAllowedMethod("GET");
         corsConfig.addAllowedMethod("POST");
